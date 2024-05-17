18f2b4e1b1ba62eca31b3c19d255f6d4a097eb50
Igor Cotruta <hugoberry314@gmail.com>
2024-05-17 09:41:11 +0100

    setup pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbixray',
-    version='0.1.10',
+    version='0.1.11',
     packages=find_packages(),
     install_requires=[
         'kaitaistruct',
@@ -12,5 +12,29 @@ setup(
     include_package_data=True,
     package_data={
         'pbixray': ['lib/*.dll', 'lib/*.so', 'lib/*.dylib'],
-    }
+    },
+    author="Igor Cotruta",
+    description="A Python library to parse and analyze PBIX files used with Microsoft Power BI.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Hugoberry/pbixray",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
