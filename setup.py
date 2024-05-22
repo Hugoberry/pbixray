b83ea25a9ac72af2c47c0152828f8e5770d99dc3
Igor Cotruta <hugoberry314@gmail.com>
2024-05-22 10:52:14 +0100

    bump v0.1.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbixray',
-    version='0.1.13',
+    version='0.1.14',
     packages=find_packages(),
     install_requires=[
         'kaitaistruct',
