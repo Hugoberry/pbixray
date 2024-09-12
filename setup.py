8b5803477b02d31319166e7a3cde766044d79902
Igor Cotruta <hugoberry314@gmail.com>
2024-09-12 17:20:43 +0100

    adding relationships

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbixray',
-    version='0.1.15',
+    version='0.1.17',
     packages=find_packages(),
     install_requires=[
         'kaitaistruct',
