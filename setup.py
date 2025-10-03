0aa1a218fb8a76c8759888d32a303511b1e36590
Igor Cotruta <hugoberry314@gmail.com>
2025-10-03 17:35:47 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbixray',
-    version='0.3.3',
+    version='0.4.0',
     packages=find_packages(),
     install_requires=[
         'xpress9',
