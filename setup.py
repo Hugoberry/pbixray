c875cc94ef6dfcd169c3b991480f56be7e2ac934
Igor Cotruta <hugoberry314@gmail.com>
2026-05-10 15:14:12 +0100

    bump perf improved version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbixray',
-    version='0.7.0',
+    version='0.8.0',
     packages=find_packages(),
     install_requires=[
         'xpress9',
