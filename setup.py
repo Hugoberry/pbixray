330b1daf5a65dac2472d05c1f7211da2595bc982
Igor Cotruta <hugoberry314@gmail.com>
2026-07-01 23:28:44 +0100

    mmap optimizations version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbixray',
-    version='0.14.0',
+    version='0.15.0',
     packages=find_packages(),
     install_requires=[
         'xpress8',
